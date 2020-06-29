30778885eebb66185bfc10a9d2816cc793a72dee
hualalalalalala <490291490@qq.com>
2020-06-29 14:56:52 +0800

    test

--- github3st/src/page/Battle/Combination.js
+++ github3st/src/page/Battle/Combination.js
@@ -49,7 +49,7 @@ class Combination extends React.Component {
               <Result player1={player1} player2={player2} reset={this.reset} />
               )
         }
-        return <Battle startBattle={this.startBattle} />
+        return <Battle startBattle={this.startBattle} ///>
     }
 }
 export default Combination;
